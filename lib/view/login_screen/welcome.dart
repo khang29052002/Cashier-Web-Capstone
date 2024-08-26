aa5bac3f83534467355fbd9e13a4883c3822963f
khangnh2905 <97997915+khangnh2905@users.noreply.github.com>
2024-08-26 18:18:32 +0700

    login ui without api

--- lib/view/login_screen/welcome.dart
+++ lib/view/login_screen/welcome.dart
@@ -1,101 +1,279 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:rive/rive.dart' as rive;
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void switchToForgotPasswordTab() {
+    _tabController.animateTo(1); // Switch to the Forgot Password tab
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Row(
         children: [
-          Image.asset(
-            'assets/images/Logo.png',
-            width: 180.0,
-            height: 180.0,
-          ),
-          SizedBox(height: 4.0), 
-          Text(
-            'Welcome to Vega City',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 40.0, 
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
+          // Animated Section
+          Expanded(
+            flex: 1,
+            child: rive.RiveAnimation.asset(
+              'assets/images/animation_login.riv',
+              fit: BoxFit.cover,
             ),
           ),
-          SizedBox(height: 40.0), 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Image.asset(
-                  'assets/images/cash.png',
-                  width: 150.0,
-                  height: 150.0,
+          // Form Section
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/Logo.png',
+                    width: 120.0,
+                    height: 120.0,
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Welcome to Vega City',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 24.0),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        // TabBar
+                        TabBar(
+                          controller: _tabController,
+                          indicator: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25.0),
+                            gradient: LinearGradient(
+                              colors: [Colors.blueAccent, Colors.lightBlueAccent],
+                            ),
+                          ),
+                          labelColor: Colors.white,
+                          unselectedLabelColor: Colors.black,
+                          labelStyle: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          unselectedLabelStyle: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.normal,
+                          ),
+                          tabs: [
+                            Tab(
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  color: _tabController.index == 0
+                                      ? Colors.blueAccent.withOpacity(0.1)
+                                      : Colors.transparent,
+                                ),
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text('Login'),
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  color: _tabController.index == 1
+                                      ? Colors.blueAccent.withOpacity(0.1)
+                                      : Colors.transparent,
+                                ),
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text('Forgot Password'),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        SizedBox(height: 15.0),
+                        Container(
+                          height: 350,
+                          child: TabBarView(
+                            controller: _tabController,
+                            children: [
+                              _buildLoginForm(),
+                              _buildForgotPasswordForm(),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoginForm() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 340,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                labelStyle: TextStyle(color: Colors.black45),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(12.0),
                 ),
               ),
-              SizedBox(width: 20.0), 
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.all(32.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          labelStyle: TextStyle(color: Colors.black45),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20.0),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          labelStyle: TextStyle(color: Colors.black45),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                        ),
-                      ),
-                       SizedBox(height: 40.0),
-                      ElevatedButton(
-                        onPressed: () {
-                          Get.offNamed('/home');
-                        },
-                        child: Text(
-                          'Login',
-                          style: TextStyle(fontSize: 16.0), 
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 100.0), 
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0), 
-                          ),
-                        ),
-                      ),
-                    ],
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            width: 340,
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(color: Colors.black45),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 30.0),
+          ElevatedButton(
+            onPressed: () {
+              Get.offNamed('/home');
+            },
+            child: Text('Login', style: TextStyle(fontSize: 16.0)),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 80.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 20.0),
+          if (_tabController.index == 0) // Show only when Login tab is selected
+            Align(
+              alignment: Alignment.center,
+              child: TextButton(
+                onPressed: () {
+                  switchToForgotPasswordTab();
+                },
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-            ],
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildForgotPasswordForm() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 340,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                labelStyle: TextStyle(color: Colors.black45),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 30.0),
+          ElevatedButton(
+            onPressed: () {
+              // Implement forgot password logic here
+            },
+            child: Text('Reset Password', style: TextStyle(fontSize: 16.0)),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 80.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
           ),
         ],
       ),
